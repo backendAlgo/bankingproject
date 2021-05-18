23f52b8c09862e27d949713291dada039558219e
Mirshod <mirshod.mirjonov@mail.ru>
2021-05-18 22:14:48 +0500

    initial commit

---
+++ src/test/java/com/mprodev/jobdemo/JobdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mprodev.jobdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
